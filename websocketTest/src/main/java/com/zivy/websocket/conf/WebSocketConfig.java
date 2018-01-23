e4b1a6f6e5f6c9ab42540d3b89a17d00c4d44614
hongcai <37494097@qq.com>
2018-01-23 16:01:28 +0800

    注解

--- websocketTest/src/main/java/com/zivy/websocket/conf/WebSocketConfig.java
+++ websocketTest/src/main/java/com/zivy/websocket/conf/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
+ * 如果用默认容器。这个可以不需要
 * @author zivy
 * @date 2018年1月23日
 * @describe
